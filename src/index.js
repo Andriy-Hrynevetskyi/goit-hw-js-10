5bc886b200d789ac577bc6e96d1efbc432f82bde
unknown <andrijgrineveckij@gmail.com>
2022-06-10 19:41:06 +0300

    add debounce

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
+import debounce from 'lodash.debounce';
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+// ?fields=name,capital,currencies
+
+const refs = {
+  input: document.querySelector('#search-box'),
+  countriesList: document.querySelector('.country-list'),
+  countryInfo: document.querySelector('.country-info'),
+};
+
+refs.input.addEventListener('input', debounce(onInputChange, DEBOUNCE_DELAY));
+
+function onInputChange(event) {
+  let country = event.target.value;
+
+  console.log(country);
+
+  fetchCountries(country);
+}
+
+function fetchCountries(country) {
+  return fetch(`https://restcountries.com/v2/name/${country}`).then(
+    response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    }
+  );
+}
